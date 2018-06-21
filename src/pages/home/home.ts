ef8d5a04142b9eed4e75c5aa1e777531ba8864f7
NitaiFraire <fernandezfrairenitai@hotmail.com>
2018-06-21 16:22:58 -0500

    Aplicacion  sonidos de animales finalizada

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,91 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+//Se importa la data
+import {ANIMALES} from "../../data/data.animales";
+
+import {Animal} from "../../interfaces/animal.interface";
+
+import {Refresher, reorderArray} from "ionic-angular";
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage
+{
+  //copia de la data
+   animales:Animal[] = [];
+   audio = new Audio();
+   audioTiempo: any;
+   ordenando: boolean = false;
 
-  constructor(public navCtrl: NavController) {
 
+  constructor()
+  {
+      //Se toma la referencia al item animales
+      //splice para que los objetos sean diferentes
+      this.animales = ANIMALES.slice(0);
+  }
+
+  reproducir(animal: Animal)
+  {
+
+    this.pausarAudio(animal);
+
+    if(animal.reproduciendo)
+    {
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log(animal);
+
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.audioTiempo = setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000);
+  }
+
+  private pausarAudio(animalSeleccionado: Animal)
+  {
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for(let animal of this.animales)
+    {
+      if(animal.nombre != animalSeleccionado.nombre)
+      {
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  borrarAnimal(idx: number)
+  {
+    this.animales.splice(idx, 1);
+  }
+
+  recargarAnimales(refresher: Refresher)
+  {
+    console.log("Inicio del refresh");
+    setTimeout(()=>{
+
+        console.log("Termino el refresh");
+        this.animales = ANIMALES.slice(0);
+        refresher.complete();
+
+    },1500)
+  }
+
+  reordenarAnimales(indices: any )
+  {
+    console.log(indices);
+    this.animales = reorderArray(this.animales, indices);
   }
 
 }
